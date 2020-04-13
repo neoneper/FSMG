8b5e838af28282fcff9f5a65c27852c7dd0452c3
Julio Pereira <juliocp.next@gmail.com>
2020-04-13 12:08:53 -0300

    Removed InitName from action nodes

--- Behaviour/Nodes/Node_ActionCustom.cs
+++ Behaviour/Nodes/Node_ActionCustom.cs
@@ -9,9 +9,7 @@ namespace XNode.FSMG
     [CreateNodeMenu("Actions/Custom")]
     public class Node_ActionCustom : NodeBase_Action
     {
-        //usado somente para renomear o nó assim que é criado, para evitar o nome feio vindo do arquivo.cs
-        private bool isAlreadyRename = false;
-
+      
         [Output]
         public NodeBase_Action outAction = null;
 
@@ -19,40 +17,12 @@ namespace XNode.FSMG
         private AI_ActionBase action = null;
 
 
-        protected override void Init()
-        {
-            //Renomeia o nó na primeira vez que o nó é iniciado, isto previne o nome feio original do .cs
-            if (!isAlreadyRename)
-                this.name = "Action";
-
-            isAlreadyRename = true;
-
-            base.Init();
-        }
         public override void Execute(FSMBehaviour fsm)
         {
             if (action != null)
                 action.Execute(fsm);
 
-        }
-
-        //Modificando as propriedades da label de conexões
-        public override string GetNoodleLabel()
-        {
-            //Quero mostrar o nome da ação anexada ao nó caso ela exista
-            string label = FSMGUtility.StringTag_Undefined;
-            if (action != null) label = action.name;
-            return label;
-
-        }
-        public override INodeNoodleLabelActiveType GetNoodleLabelActive()
-        {
-            //Esonde a label caso nao haja nenhuma ação referenciada
-
-            if (action == null)
-                return INodeNoodleLabelActiveType.Never;
-            else return INodeNoodleLabelActiveType.SelectedPair;
-        }
+        }       
 
     }
 }
